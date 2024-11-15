79fd05a5b6a1a4ace54ed40c3ac3716ae4a38f27
Zama <zamaslindy09@gmail.com>
2024-11-16 00:14:39 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,10 @@ function formateDate(date) {
     let day = days[date.getDay()];
 
 if (minutes < 10) {
-    minutes = `0 ${minutes}`;
+    minutes = `0${minutes}`;
+}
+if (hours <10) {
+    hours = `0${hours}`;
 }
     
     return `${day} ${hours}:${minutes}`;
