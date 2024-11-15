be83f78104c8b8d7a20cabd364c6b6b5e1931a5c
Zama <zamaslindy09@gmail.com>
2024-11-15 22:18:38 +0200

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,10 +2,32 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
 
-    cityElement.innerHTML= response.data.city;
+    timeElement.innerHTML = formateDate(date);
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    descriptionElement.innerHTML = response.data.condition.description;
+    cityElement.innerHTML = response.data.city;
     temperatureElement.innerHTML = Math.round(temperature);
    }
+
+function formateDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    let day = days[date.getDay()];
+
+if (minutes < 10) {
+    minutes = `0:(minutes)`;
+}
+    
+    return `${day} ${hours}:${minutes}`;
+}
 
    function searchCity(city) {
     let apiKey = "e10304dcb3a23oc6fa4fbe32t589a6ae";
